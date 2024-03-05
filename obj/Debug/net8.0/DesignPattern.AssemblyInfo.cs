4df1ddac1ed8f4aa2af56aec64989456cc6e469c
yjq <2765248414@qq.com>
2024-03-06 00:21:54 +0800

    wpf update

--- obj/Debug/net8.0/DesignPattern.AssemblyInfo.cs
+++ obj/Debug/net8.0/DesignPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83a959f076e6fffd61ca5d8e0978f3f8a7b7630d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34eb5089958d7fb833cc7fb92e12475174b55571")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
