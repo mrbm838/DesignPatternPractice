3dc0ac41ed1954e33f2a771521a5bae55be83d99
yjq <2765248414@qq.com>
2024-03-08 00:12:27 +0800

    Add AdapterPattern class for implementing the Adapter design pattern

---
+++ AdapterPattern.cs
@@ -0,0 +1,87 @@
+﻿namespace DesignPattern;
+
+public class AdapterPattern
+{
+    public static void Main()
+    {
+        Player b = new Forwards("巴蒂尔");
+        b.Attack();
+
+        Player m = new Center("麦克格雷迪");
+        m.Attack();
+
+        Player ym = new Translator("姚明");
+        ym.Attack();
+        ym.Defense();
+    }
+}
+
+abstract class Player
+{
+    public string Name { get; set; }
+
+    public Player(string name)
+    {
+        Name = name;
+    }
+
+    public abstract void Attack();
+    public abstract void Defense();
+}
+
+class Forwards(string name) : Player(name)
+{
+    public override void Attack()
+    {
+        Console.WriteLine($"前锋 {Name} 进攻");
+    }
+
+    public override void Defense()
+    {
+        Console.WriteLine($"前锋 {Name} 防守");
+    }
+}
+
+class Center(string name) : Player(name)
+{
+    public override void Attack()
+    {
+        Console.WriteLine($"中锋 {Name} 进攻");
+    }
+
+    public override void Defense()
+    {
+        Console.WriteLine($"中锋 {Name} 防守");
+    }
+}
+
+class ForeignCenter
+{
+    public required string Name { get; set; }
+    public void 进攻()
+    {
+        Console.WriteLine($"外籍中锋 {Name} 进攻");
+    }
+
+    public void 防守()
+    {
+        Console.WriteLine($"外籍中锋 {Name} 防守");
+    }
+}
+
+class Translator : Player
+{
+    private ForeignCenter wjzf;
+
+    public Translator(string name) : base(name) => wjzf = new ForeignCenter { Name = name };
+
+    public override void Attack()
+    {
+        wjzf.进攻();
+    }
+
+    public override void Defense()
+    {
+        wjzf.防守();
+    }
+}
--- obj/Debug/net8.0/DesignPattern.AssemblyInfo.cs
+++ obj/Debug/net8.0/DesignPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df1ddac1ed8f4aa2af56aec64989456cc6e469c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdbe9bb271d4c1b6e47282f95243a4343ce3f29a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
