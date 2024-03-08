2afcc8f072d3d2adeb5e64e4f3099cf9f296d93f
yjq <2765248414@qq.com>
2024-03-08 11:16:32 +0800

    Add CompositePattern and MementoPattern classes

---
+++ CompositePattern.cs
@@ -0,0 +1,87 @@
+﻿namespace DesignPattern;
+
+public class CompositePattern
+{
+    public static void Main()
+    {
+        Composite root = new Composite("root");
+        root.Add(new Leaf("Leaf A"));
+        root.Add(new Leaf("Leaf B"));
+
+        Composite comp = new Composite("Composite X");
+        comp.Add(new Leaf("Leaf XA"));
+        comp.Add(new Leaf("Leaf XB"));
+
+        root.Add(comp);
+
+        Composite comp2 = new Composite("Composite XY");
+        comp2.Add(new Leaf("Leaf XYA"));
+        comp2.Add(new Leaf("Leaf XYB"));
+
+        comp.Add(comp2);
+
+        root.Add(new Leaf("Leaf C"));
+
+        Leaf leaf = new Leaf("Leaf D");
+        root.Add(leaf);
+        root.Remove(leaf);
+
+        root.Display(1);
+    }
+}
+
+public abstract class Component
+{
+    protected string name;
+
+    public Component(string name)
+    {
+        this.name = name;
+    }
+
+    public abstract void Add(Component c);
+    public abstract void Remove(Component c);
+    public abstract void Display(int depth);
+}
+
+public class Leaf(string name) : Component(name)
+{
+    public override void Add(Component c)
+    {
+        Console.WriteLine("Cannot add to a leaf");
+    }
+
+    public override void Remove(Component c)
+    {
+        Console.WriteLine("Cannot remove from a leaf");
+    }
+
+    public override void Display(int depth)
+    {
+        Console.WriteLine(new string('-', depth) + name);
+    }
+}
+
+public class Composite(string name) : Component(name)
+{
+    private List<Component> children = new List<Component>();
+
+    public override void Add(Component c)
+    {
+        children.Add(c);
+    }
+
+    public override void Remove(Component c)
+    {
+        children.Remove(c);
+    }
+
+    public override void Display(int depth)
+    {
+        Console.WriteLine(new string('-', depth) + name);
+        foreach (Component component in children)
+        {
+            component.Display(depth + 2);
+        }
+    }
+}
---
+++ MementoPattern.cs
@@ -0,0 +1,57 @@
+﻿namespace DesignPattern;
+
+public class MementoPattern
+{
+    public static void Main()
+    {
+        Originator o = new Originator();
+        o.State = "On";
+        o.Show();
+
+        Caretaker c = new Caretaker();
+        c.Memento = o.CreateMemento();
+
+        o.State = "Off";
+        o.Show();
+
+        o.SetMemento(c.Memento);
+        o.Show();
+    }
+
+}
+
+public class Originator
+{
+    public string State { get; set; }
+
+    public Memento CreateMemento()
+    {
+        return new Memento(State);
+    }
+
+    public void SetMemento(Memento memento)
+    {
+        State = memento.State;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine($"State = {State}");
+    }
+}
+
+public class Memento
+{
+    public string State { get; }
+
+    public Memento(string state)
+    {
+        State = state;
+    }
+}
+
+public class Caretaker
+{
+    public Memento Memento { get; set; }
+}
+
