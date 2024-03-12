8cc7c57ebb985b78ee380722894489ecacf2aa13
yjq <2765248414@qq.com>
2024-03-13 00:41:51 +0800

    Add TemplateFunctionPattern class and update AssemblyInfo version

---
+++ TemplateFunctionPattern.cs
@@ -0,0 +1,55 @@
+﻿namespace DesignPattern;
+
+public class TemplateFunctionPattern
+{
+    public static void Main(string[] args)
+    {
+        AbstractClass c;
+
+        c = new ConcreteClassA();
+        c.TemplateMethod();
+
+        c = new ConcreteClassB();
+        c.TemplateMethod();
+
+        Console.ReadKey();
+    }
+}
+
+abstract class AbstractClass
+{
+    public abstract void PrimitiveOperation1();
+    public abstract void PrimitiveOperation2();
+
+    public void TemplateMethod()
+    {
+        PrimitiveOperation1();
+        PrimitiveOperation2();
+    }
+}
+
+class ConcreteClassA : AbstractClass
+{
+    public override void PrimitiveOperation1()
+    {
+        System.Console.WriteLine("ConcreteClassA.PrimitiveOperation1()");
+    }
+
+    public override void PrimitiveOperation2()
+    {
+        System.Console.WriteLine("ConcreteClassA.PrimitiveOperation2()");
+    }
+}
+
+class ConcreteClassB : AbstractClass
+{
+    public override void PrimitiveOperation1()
+    {
+        System.Console.WriteLine("ConcreteClassB.PrimitiveOperation1()");
+    }
+
+    public override void PrimitiveOperation2()
+    {
+        System.Console.WriteLine("ConcreteClassB.PrimitiveOperation2()");
+    }
+}
--- obj/Debug/net8.0/DesignPattern.AssemblyInfo.cs
+++ obj/Debug/net8.0/DesignPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb262a07c8cdc003785f6f67405ece267248f53a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52827ccc18b7e2f4ca98cf3090a21b8093435f32")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
